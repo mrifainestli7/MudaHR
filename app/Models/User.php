78b0f83ec06b5543f24dd044ec8c3574053e9586
mrifainestli7 <mrifainestli4@gmail.com>
2025-02-15 23:19:09 +0700

    feat: install breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin', // Pastikan ini ada di database
     ];
 
     /**
@@ -44,5 +45,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Menentukan apakah pengguna dapat mengakses Filament.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->is_admin; // Hanya admin yang bisa masuk ke Filament
     }
 }
